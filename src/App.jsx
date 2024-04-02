83b378f910be97398881649447cb05cc48296174
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2024-04-02 12:49:05 +0600

    success story section design complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import About from "./components/about";
 import Header from "./components/header";
 import NavigationBar from "./components/navigation-bar";
 import QualificationAwards from "./components/qualification-awards";
+import SensitivePerson from "./components/sensitive-person";
+import SuccessStory from "./components/success-story";
 import Testimonial from "./components/testimonial";
 import WorkHistory from "./components/work-history";
 
@@ -14,6 +16,8 @@ const App = () => {
       <WorkHistory />
       <Testimonial />
       <QualificationAwards />
+      {/* <SensitivePerson /> */}
+      <SuccessStory />
     </>
   );
 };
